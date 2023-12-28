04b393d2d226b373744ff7e67b66073e47b4bb7d
Sentting <1762967935@qq.com>
2023-12-29 00:26:21 +0800

    1

--- num/num4.py
+++ num/num4.py
@@ -1,10 +1,10 @@
 # from requests_util import RequestUtil
 from yaml_util import read_yaml_testcase
 
-date = read_yaml_testcase("num.yaml")
+date = read_yaml_testcase("num.yam")
 print(date)
 # currentTime = time.localtime()
 # date_time = str(time.strftime("%Y-%m-%d %H:%M:%S", currentTime))
 # json = read_yaml('json')
 # json['push_time'] = date_time
-# print(json)
+# print(json)cs
